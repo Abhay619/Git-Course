6656656280c2cf7b0bad26f1c311b7154553e212
Abhay Kumar Dhuriya <Abhayk6190@gmail.com>
2024-12-17 21:30:06 +0530

    second commit

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const names = "Abhay";
+const names = "Aradhna";
 
 const age = 21;
 
 function sayName() {
     console.log("My name is "+ names);
+}
+
+function isAbove18() {
+    return age >= 18;
 }
